066a11e9eb0ed8ca8ef61f69e1538964ee2a3bfe
umidbeksalayev <umidbeksalayev13@gmail.com>
2025-04-05 22:15:56 +0500

    ProductResource, CategoryResource and Stockresource created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CategoryProductController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,5 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResources([
     'categories' => CategoryController::class,
     'products' => ProductController::class,
+    'categories.products' => CategoryProductController::class,
 
 ]);
